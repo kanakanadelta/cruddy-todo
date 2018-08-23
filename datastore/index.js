63fc24eb5e90d47132e3534db6b0c1b0aa8749fc
kanakanadelta <kanakanadelta@gmail.com>
2018-08-23 11:15:26 -0700

    starting readOne

--- datastore/index.js
+++ datastore/index.js
@@ -8,38 +8,43 @@ var items = {};
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-//   var id = counter.getNextUniqueId();
-//   items[id] = text;
-//   callback(null, {id: id, text: text});
-// };
-
-// exports.readOne = (id, callback) => {
-//   var item = items[id];
-//   if (!item) {
-//     callback(new Error(`No item with id: ${id}`));
-//   } else {
-//     callback(null, {id: id, text: item});
-//   }
-
+  //invoke getNextUniqueId to retrieve id from counter
   counter.getNextUniqueId((err, id) => {
-
-    console.log(id)
+    //since we are in scope, we are able to get the id, and write a new file with given padded id.
     fs.writeFile(path.join(exports.dataDir,`${id}.txt`), text, (err) => {
+      //error-first callback pattern initiate - node.js standard practice
       if (err) {
         callback(err);
       } else {
         callback(null, {id: id, text: text});
-      }   
+      }
     });
   });
 };
 
+//check if todo-item is written in the directory it is looking for
+exports.readOne = (id, callback) => {
+  //no longer using the items object, instead use DataDir
+  // var item = items[id];
+
+  fs.readFile(path.join(exports.dataDir,`${id}.txt`), text, (err) => {
+    //error-first callback pattern initiate - node.js standard practice
+    if (err) {
+      callback(err);
+    } else if (!item) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback(null, {id: id, text: item});
+    }
+  })
+};
+
 exports.readAll = (callback) => {
-  // var data = [];
-  // _.each(items, (item, idx) => {
-  //   data.push({ id: idx, text: items[idx] });
-  // });
-  // callback(null, data);
+  var data = [];
+  _.each(items, (item, idx) => {
+    data.push({ id: idx, text: items[idx] });
+  });
+  callback(null, data);
 
   //iterate through our data folder / "todo list"
     //retrieve length?
